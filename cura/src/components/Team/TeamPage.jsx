9921d03e71560ce478a013b28248d85288f313e0
Salim Znaidi <znaidisal@gmail.com>
2025-03-17 02:42:14 +0100

    fix: cambio del layout da grid a flex

--- cura/src/components/Team/TeamPage.jsx
+++ cura/src/components/Team/TeamPage.jsx
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 
 export default function TeamPage() {
   const teamMembers = [
-    // Crea l'array di oggetti con le informazioni dei membri del team
     {
       id: 1,
       name: "Salim Znaidi",
-      image: "path/to/salim.png",
+      image: "/public/salim.png",
       description: "descrizione di Salim",
     },
     {
@@ -38,26 +37,31 @@ export default function TeamPage() {
   const [selectedMember, setSelectedMember] = useState(null); // Usa selectedMember in modo coerente
 
   return (
-    <div className="team-page bg-[#A1C877] h-screen flex justify-center items-center">
-      <div className="grid grid-cols-3 gap-8">
+    <div className="team-page bg-[#A1C877] h-screen flex flex-col justify-center items-center py-8">
+      <h2 className="text-6xl font-[Titan One] text-[#205F72] mb-50">
+        Incontra il nostro team
+      </h2>
+      {/* titolo */}
+      <div className="flex flex-wrap justify-center gap-30">
         {teamMembers.map((member) => (
           <div
             key={member.id}
-            className="team-member bg-gradient from-blue-400 via-green-500 to-yellow-500 p-4 rounded-2x1 cursor-pointer transition-transform duration-300 hover:scale-105"
-            onClick={() => setSelectedMember(member)} // Aggiorna lo stato qui
+            className="team-member flex flex-col items-center bg-[#A1C877] p-4 rounded-xl cursor-pointer transition-transform duration-300 hover:scale-105"
+            onClick={() => setSelectedMember(member)} // event handler per il click sull'immagine del Team member
           >
-            <div className="member-image overflow-hidden relative">
+            <div className="member-image overflow-hidden relative bg-gradient-to-b from-[#205F72] to-[#A1C877] p-4 rounded-full">
               <img
                 src={member.image}
                 alt={member.name}
-                className="w-32 h-32 object-cover rounded-full border-4 border-white absolute -top-8 left-1/2 transform -translate-x-1/2"
+                className="w-32 h-32 object-cover rounded-full"
               />
             </div>
-            <h3 className="text-center text-xl mt-16 text-white">
+            <h3 className="text-center text-xl mt-4 text-[#205F72]">
               {member.name}
             </h3>
-            {selectedMember?.id === member.id && ( // Usa selectedMember qui
-              <p className="mt-2 text-center text-white">
+
+            {selectedMember?.id === member.id && (
+              <p className="mt-2 text-center text-[#205F72]">
                 {member.description}
               </p>
             )}
