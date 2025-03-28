025f8e2e2598f7af2adf86ee2951ec1e0fc2e7ed
[cristianbovenzi]git config --global user.name [cristianbovenzi] <[cristianbovenzi@gmail.com]>
2025-03-28 10:59:16 +0100

    Fix bug

--- cura/src/components/Sezione-Quiz/Quiz-Categorie.jsx
+++ cura/src/components/Sezione-Quiz/Quiz-Categorie.jsx
@@ -3,11 +3,11 @@ import QuizCard from "../Card-Quiz/Quiz-Card";
 export default function CategorieQuiz() {
   return (
     <div className=" grid-cols-1 bg-[#A1C877] rounded-2xl p-0.5 col-span-2 pt-10 overflow-auto list">
-      <QuizCard title="VITA LAVORO" hand={hands} className="quiz"/>
-      <QuizCard title="SONNO E RECUPERO" hand={hands} className="quiz"/>
-      <QuizCard title="LIVELLO DI STRESS" hand={hands} className="quiz"/>
-      <QuizCard title="UMORE ED EMOZIONI" hand={hands} className="quiz"/>
-      <QuizCard title="AUTOCONSAPEVOLEZZA" hand={hands} className="quiz"/>
+      <QuizCard title="VITA LAVORO" className="quiz" />
+      <QuizCard title="SONNO E RECUPERO" className="quiz" />
+      <QuizCard title="LIVELLO DI STRESS" className="quiz" />
+      <QuizCard title="UMORE ED EMOZIONI" className="quiz" />
+      <QuizCard title="AUTOCONSAPEVOLEZZA" className="quiz" />
     </div>
   );
 }
