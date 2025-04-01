e836758ab074f91e3b933d935b32db0c96ce1861
CryptoFamilyNFT <95889492+CryptoFamilyNFT@users.noreply.github.com>
2025-04-01 17:02:35 +0200

    feat: implemented fetchQuizData() in quizPage

--- cura/src/App.jsx
+++ cura/src/App.jsx
@@ -6,16 +6,22 @@ import { Navbar } from "./components/Navbar/Navbar";
 import SezioneQuiz from "./components/Sezione-Quiz/Sezione-Quiz";
 import { Route, Routes } from "react-router";
 import TeamPage from "./components/Team/TeamPage";
+import QuizPage from "./pages/QuizPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="Login" />
-      <Route path="Quiz" element={<SezioneQuiz />} />
-      <Route path="team" element={<TeamPage />} />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="Login" />
+        <Route path="Quiz" element={<SezioneQuiz />} />
+        <Route path="team" element={<TeamPage />} />
+        <Route path="/" element={<Hero />} />
+        <Route path="quizpage" element={<QuizPage />} />
+        <Route path="SignUp" element={<SignUp />} />
 
-    </Routes >
-
+      </Routes >
+    </>
   );
 }
 
