0a62caae37ab17048b76e54cfbcc699af785853a
Andrea <andrea.seidita00@gmail.com>
2025-04-01 18:08:14 +0200

    fix: profilepage

--- cura/src/App.jsx
+++ cura/src/App.jsx
@@ -6,6 +6,7 @@ import { Navbar } from "./components/Navbar/Navbar";
 import SezioneQuiz from "./components/Sezione-Quiz/Sezione-Quiz";
 import { Route, Routes } from "react-router";
 import TeamPage from "./components/Team/TeamPage";
+import ProfilePage from "./pages/ProfilePage";
 
 import QuizPage from "./pages/QuizPage";
 
@@ -20,9 +21,8 @@ function App() {
         <Route path="/" element={<Hero />} />
         <Route path="quizpage" element={<QuizPage />} />
         <Route path="SignUp" element={<SignUp />} />
-      <Route path="profile" element={<ProfilePage />} />
-
-      </Routes >
+        <Route path="profile" element={<ProfilePage />} />
+      </Routes>
     </>
   );
 }
