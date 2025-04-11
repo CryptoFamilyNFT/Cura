a7307ea17e31fe4a6dfb26850ce08c4752ead04d
[cristianbovenzi]git config --global user.name [cristianbovenzi] <[cristianbovenzi@gmail.com]>
2025-04-11 11:18:14 +0200

    Apportato modifiche al codice

--- cura/src/App.jsx
+++ cura/src/App.jsx
@@ -2,21 +2,24 @@ import SezioneQuiz from "./components/Sezione-Quiz/Sezione-Quiz";
 import { Route, Routes } from "react-router";
 import TeamPage from "./components/Team/TeamPage";
 import ProfilePage from "./pages/ProfilePage";
-
+import HomeReview from "./components/Review/HomeReview"
 import QuizPage from "./pages/QuizPage";
+import FeedbackPage from "./pages/FeedbackPage";
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="Login" />
         <Route path="Quiz" element={<SezioneQuiz />} />
         <Route path="team" element={<TeamPage />} />
-        <Route path="/" element={<Hero />} />
-        <Route path="quizpage" element={<QuizPage />} />
-        <Route path="SignUp" element={<SignUp />} />
+        {/* <Route path="/" element={<Hero />} /> */}
+        <Route path="QuizPage" element={<QuizPage />} />
+        {/* <Route path="SignUp" element={<SignUp />} /> */}
         <Route path="profile" element={<ProfilePage />} />
+        <Route path="/Review" element={<HomeReview />} />
+        <Route path="/Feedback" element={<FeedbackPage />} />
       </Routes>
     </>
   );
