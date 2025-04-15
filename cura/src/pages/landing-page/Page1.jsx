ab5d6faa313a708ac502f0fdaaa6d4ae099e71a4
[cristianbovenzi]git config --global user.name [cristianbovenzi] <[cristianbovenzi@gmail.com]>
2025-04-15 16:19:19 +0200

    Migliorie codice

--- cura/src/pages/landing-page/Page1.jsx
+++ cura/src/pages/landing-page/Page1.jsx
@@ -21,7 +21,7 @@ const Quizinterattivi = () => {
             <div className="flex flex-col w-screen items-center text-center">
                 <h1 className="absolute top-10 left-15 text-[#23687D] text-9xl font-bold">01</h1>
                 <h2 className="text-[#23687D] text-5xl mt-60 max-w-lg break-words text-right w-[420px] py-4 ">QUIZ INTERATTIVI PERSONALIZZATI </h2>
-                <p className="text-[#23687D] text-2xl max-w-lg mt-4 w-full text-right -ml-23 font-[comfortaa] ">
+                <p className="text-[#23687D] text-2xl max-w-lg mt-4 w-full text-right -ml-23">
                 Grazie all'analisi dell'AI, ogni quiz ti offre un percorso su misura per migliorare il tuo benessere. Rispondi alle domande, esplora i risultati e ricevi consigli pratici per una vita più serena e soddisfacente.
                 </p>
             </div>
